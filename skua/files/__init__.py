12a2743ae88030faa995cae3563e1efc7bef2169
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-08-23 10:11:59 +0100

    Updated `skua.files.generate_output_files` so that nested files generate properly. [ci skip]

--- skua/files/__init__.py
+++ skua/files/__init__.py
@@ -6,4 +6,9 @@ def generate_output_filenames(input_files: List[str], source_dir: str, output_di
     for file in input_files:
         path = pathlib.Path(file)
         index = path.parts.index(source_dir) + 1
-        yield str(pathlib.Path(output_dir).joinpath(*path.parts[index:]))
+        pre_index = index - 2
+        if pre_index >= 0:
+            yield str(
+                pathlib.Path('').joinpath(*path.parts[:pre_index]).joinpath(output_dir).joinpath(*path.parts[index:]))
+        else:
+            yield str(pathlib.Path(output_dir).joinpath(*path.parts[index:]))
--- tests/files/__init__.py
+++ tests/files/__init__.py
@@ -4,10 +4,21 @@ from skua.files import generate_output_filenames
 
 
 class TestOutputFilenameGenerator(unittest.TestCase):
-    def test1(self):
+    def test_no_nesting(self):
         output = generate_output_filenames(
             ['src/index.md', 'src/blog/skua-is-a-static-file-generator.md', 'src/LICENSE.md'],
             'src', 'build')
         expectation = ['build/index.md', 'build/blog/skua-is-a-static-file-generator.md', 'build/LICENSE.md']
+        for (y_hat, y) in zip(output, expectation):
+            self.assertTrue(y_hat == y)
+
+    def test_nesting(self):
+        output = generate_output_filenames(
+            ['project/docs/src/index.md', 'project/docs/src/blog/skua-is-a-static-file-generator.md',
+             'project/docs/src/LICENSE.md'],
+            'src', 'build')
+        expectation = ['project/docs/build/index.md', 'project/docs/build/blog/skua-is-a-static-file-generator.md',
+                       'project/docs/build/LICENSE.md']
+        print(list(output))
         for (y_hat, y) in zip(output, expectation):
             self.assertTrue(y_hat == y)
