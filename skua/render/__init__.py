4dec941811d9e4cbd7baf2dcd382888fd7b6e3bc
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-08-31 11:46:16 +0100

    Updated docstrings in `skua.render.Templates`. [ci skip]

--- skua/render/__init__.py
+++ skua/render/__init__.py
@@ -11,15 +11,21 @@ class Templates(object):
     def __init__(self, template_dir: pathlib.Path, template_extension: str = 'html',
                  template_prefix: str = "skua_"):
         """
-        Stores jinja2 templates. Please note that templates need to be unique.
-        :param template_dir: The folder in which the templates can be found.
+        A thin wrapper around jinja2's template environment.
+
+        :param template_dir: The folder in which the templates are stored.
+
         :param template_extension: All files without this extension are ignored.
+
+        :param template_prefix: All folders without this prefix are ignored. A sensible default is the name of your
+        organisation.
         """
         if not template_dir.exists():
             raise LookupError("The template folder cannot be found.")
         self.env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(str(template_dir))
         )
+
         template_dir_index = [template for template in
                               glob.glob(os.path.join(os.path.abspath(str(template_dir)), '**'), recursive=True) if
                               re.search(template_prefix, template) and os.path.splitext(os.path.split(template)[1])[
@@ -31,13 +37,30 @@ class Templates(object):
              template_file in template_dir_index])
 
     def render_template(self, template, **kwargs):
+        """
+        Takes a template and renders it as HTML.
+
+        :param template: The template to be used. The template should be
+        specified without the extension, e.g. to use the template `skua_blogpost.html` you would use `skua_blogpost`
+        as the value for the argument `template.
+
+        :param kwargs: Keyword arguments – these are all accessible to the jinja2 template enviroment.
+
+        :return: The HTML output of the file.
+        """
         return self.templates[template].render(**kwargs)
 
     def __call__(self, template, **kwargs):
         """
-        The __call__ method is implemented to allow this site to work with Pipelines.
-        :param template:
-        :param kwargs:
-        :return:
+        The __call__ method is implemented to allow this site to work with Pipelines. It does the exact same thing as
+        `skua.render.Templates.render_template`.
+
+        :param template: The template to be used. The template should be
+        specified without the extension, e.g. to use the template `skua_blogpost.html` you would use `skua_blogpost`
+        as the value for the argument `template.
+
+        :param kwargs: Keyword arguments – these are all accessible to the jinja2 template enviroment.
+
+        :return: The HTML output of the file.
         """
         return self.render_template(template, **kwargs)
