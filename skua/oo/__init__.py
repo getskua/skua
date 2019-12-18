0c26225f581bff4e717a8d7cf262c537574e25ad
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-18 16:34:57 +0000

    Added rendering methods (to be subclassed) to `skua.oo`.

--- skua/oo/__init__.py
+++ skua/oo/__init__.py
@@ -1,17 +1,37 @@
 import enum
+from typing import List
 
 
 class PageType(enum.Enum):
     MARKDOWN = 1
     HTML = 2
-    EBOOK = 3
+
+
+class OutputFormats(enum.Enum):
+    HTML = 1
+    EBOOK = 2
+    PDF = 3
 
 
 class Page(object):
     def __init__(self, *args, **kwargs):
+        setattr(self, 'page_type', self.page_type)
+        setattr(self, 'output_formats', self.output_formats)
+
+    def render(self):
+        """
+        :return:
+        """
         pass
 
 
 class Collection(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, pages: List[Page], page_types: PageType = PageType.MARKDOWN):
+        self.pages = pages
+
+    def output_name_generator(self, page: Page) -> str:
+        """
+        This method should be subclassed and give the output path of the document.
+        :return:
+        """
         pass
