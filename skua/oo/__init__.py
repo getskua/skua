845f2ae8f90b1ce2c8cbb6205342e0a97eb3d60c
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-18 15:55:11 +0000

    Added basic classes `Page` and `PageCollection`.

---
+++ skua/oo/__init__.py
@@ -0,0 +1,17 @@
+import enum
+
+
+class PageType(enum.Enum):
+    MARKDOWN = 1
+    HTML = 2
+    EBOOK = 3
+
+
+class Page(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class Collection(object):
+    def __init__(self, *args, **kwargs):
+        pass
